f9f3298e4aeac1e0e54a11bab6af922857e71de4
Akula-Vyshu <188323972+Akula-Vyshu@users.noreply.github.com>
2024-11-15 18:04:45 +0530

    Create Find_Age.java

---
+++ VISAA_PREP/Find_Age.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        int cha = c.nextInt();
+        System.out.println(cha-15);
+        
+    }
+}
