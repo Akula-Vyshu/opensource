ff6688d25c69a33d00af26ba1dd8f60b2d7e893a
Akula-Vyshu <188323972+Akula-Vyshu@users.noreply.github.com>
2024-11-15 18:00:47 +0530

    Create Count_NoteBooks.java

---
+++ VISAA_PREP/Count_NoteBooks.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        int N = c.nextInt();
+        System.out.println((N*1000)/(100));
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
