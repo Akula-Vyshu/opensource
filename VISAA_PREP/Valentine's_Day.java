739ae4e84485f30f390b24f148eb920dc6f47fa4
Akula-Vyshu <188323972+Akula-Vyshu@users.noreply.github.com>
2024-11-18 03:05:19 +0530

    Create Valentine's_Day.java

---
+++ VISAA_PREP/Valentine's_Day.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        int x = c.nextInt();
+        int y = c.nextInt();
+        System.out.println((int)(x/y));
+        c.close();
+    }
+}
