664c757dc05520b878da9a99c69d886323c970e6
Akula-Vyshu <188323972+Akula-Vyshu@users.noreply.github.com>
2024-11-19 00:42:18 +0530

    Create Diamond_Pattern.java

---
+++ VISAA_PREP/Diamond_Pattern.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        int n = c.nextInt();
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        c.close();
+    }
+}
